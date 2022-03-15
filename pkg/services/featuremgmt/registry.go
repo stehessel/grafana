37a80023ae5fc3683e5cbef9b693bed4fd6642eb
Kristina Durivage <kristina.durivage@grafana.com>
2022-03-15 15:49:05 -0500

    Add feature flag and scaffodling

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -213,5 +213,10 @@ var (
 			State:        FeatureStateBeta,
 			FrontendOnly: true,
 		},
+		{
+			Name:        "commandPalette",
+			Description: "Enable command palette",
+			State:       FeatureStateAlpha,
+		},
 	}
 )
