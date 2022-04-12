c403e15cc2ac4cba4985474987b57b3a1f1fbc14
bohandley <brendan-grafana@Brendans-MacBook-Pro.local>
2022-04-12 16:52:19 -0400

    add comment for adding response type text to call to  graphite /functions endpoint

--- public/app/plugins/datasource/graphite/datasource.ts
+++ public/app/plugins/datasource/graphite/datasource.ts
@@ -749,6 +749,7 @@ export class GraphiteDatasource
     const httpOptions = {
       method: 'GET',
       url: '/functions',
+      // to work around Graphite returning invalid JSON
       responseType: 'text',
     };
 
