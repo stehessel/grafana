ac4e2d77169a0592f031507b0f915204f775d311
Jack Westbrook <jack.westbrook@gmail.com>
2022-04-15 15:12:38 +0200

    refactor(toolkit): rewrite linting step to use new eslint node api

--- packages/grafana-toolkit/src/cli/tasks/plugin.build.ts
+++ packages/grafana-toolkit/src/cli/tasks/plugin.build.ts
@@ -7,7 +7,7 @@ import { promisify } from 'util';
 import globby from 'globby';
 import execa from 'execa';
 import { constants as fsConstants, promises as fs } from 'fs';
-import { CLIEngine } from 'eslint';
+import { ESLint } from 'eslint';
 import { bundlePlugin as bundleFn, PluginBundleOptions } from './plugin/bundle';
 
 const { access, copyFile } = fs;
@@ -74,8 +74,6 @@ export const versions = async () => {
 // @ts-ignore
 const typecheckPlugin = () => useSpinner('Typechecking', () => execa('tsc', ['--noEmit']));
 
-const getTypescriptSources = () => globby(resolvePath(process.cwd(), 'src/**/*.+(ts|tsx)'));
-
 // @ts-ignore
 const getStylesSources = () => globby(resolvePath(process.cwd(), 'src/**/*.+(scss|css)'));
 
@@ -104,24 +102,34 @@ export const lintPlugin = ({ fix }: Fixable = {}) =>
       }
     );
 
-    const cli = new CLIEngine({
-      configFile,
+    const eslint = new ESLint({
+      extensions: ['.ts', '.tsx'],
+      overrideConfigFile: configFile,
       fix,
       useEslintrc: false,
     });
 
-    const report = cli.executeOnFiles(await getTypescriptSources());
+    const results = await eslint.lintFiles(resolvePath(process.cwd(), 'src'));
 
     if (fix) {
-      CLIEngine.outputFixes(report);
+      await ESLint.outputFixes(results);
     }
 
-    const { errorCount, results, warningCount } = report;
-    const formatter = cli.getFormatter();
+    const { errorCount, warningCount } = results.reduce<Record<string, number>>(
+      (acc, value) => {
+        acc.errorCount += value.errorCount;
+        acc.warningCount += value.warningCount;
+        return acc;
+      },
+      { errorCount: 0, warningCount: 0 }
+    );
+
+    const formatter = await eslint.loadFormatter('stylish');
+    const resultText = formatter.format(results);
 
     if (errorCount > 0 || warningCount > 0) {
       console.log('\n');
-      console.log(formatter(results));
+      console.log(resultText);
       console.log('\n');
     }
 
